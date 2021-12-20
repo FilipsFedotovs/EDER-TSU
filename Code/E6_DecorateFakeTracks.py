26fd07f72e9b3a9e8bb72cfcdeb5dc06355cf026
Filips <fedotovs@hotmail.com>
2021-12-20 20:25:10 +0000

    Development

--- Code/E6_DecorateFakeTracks.py
+++ Code/E6_DecorateFakeTracks.py
@@ -93,7 +93,7 @@ if Mode=='R':
          print(UF.TimeStamp(),'OK, continuing then...')
    if UserAnswer=='Y':
       print(UF.TimeStamp(),'Performing the cleanup... ',bcolors.ENDC)
-      UF.EvalCleanUp(AFS_DIR, EOS_DIR, 'E6', ['E6_E6_Dec_Fake_Tracks'], "SoftUsed == \"EDER-TSU-E6\"")
+      UF.EvalCleanUp(AFS_DIR, EOS_DIR, 'E6', ['E6'], "SoftUsed == \"EDER-TSU-E6\"")
       print(UF.TimeStamp(),'Submitting jobs... ',bcolors.ENDC)
       for j in range(0,len(data)):
         for sj in range(0,int(data[j][2])):
@@ -114,120 +114,150 @@ if Mode=='R':
       print(UF.TimeStamp(), bcolors.OKGREEN+'All jobs have been submitted, please rerun this script with "--Mode C" in few hours'+bcolors.ENDC)
 if Mode=='C':
    print(UF.TimeStamp(),'Checking results... ',bcolors.ENDC)
-   #test_file=EOS_DIR+'/EDER-TSU/Data/REC_SET/E6_E6_DEC_FAKE_SEEDS_1.csv'
-   #if os.path.isfile(test_file):
-   #    print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
-   #    print(UF.TimeStamp(), bcolors.OKGREEN+"The process has been completed before, if you want to restart, please rerun with '--Mode R' option"+bcolors.ENDC)
-   #    exit()
+   test_file=EOS_DIR+'/EDER-TSU/Data/REC_SET/E6_FAKE_GENUINE_TRACKS.csv'
+   if os.path.isfile(test_file):
+       print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
+       print(UF.TimeStamp(), bcolors.OKGREEN+"The process has been completed before, if you want to restart, please rerun with '--Mode R' option"+bcolors.ENDC)
+       exit()
    bad_pop=[]
    print(UF.TimeStamp(),'Checking jobs... ',bcolors.ENDC)
-   # for j in range(0,len(data)):
-   #     for sj in range(0,int(data[j][2])):
-   #         for f in range (0,1000):
-   #            new_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E5_E6_RawTracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
-   #            required_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E6_E6_Dec_Fake_Tracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
-   #            OptionHeader = [' --Set ', ' --SubSet ', ' --EOS ', " --AFS ", " --Fraction ",' --MaxDOCA ', ' --MaxAngle ', ' --MaxSTG ', ' --MaxSLG ']
-   #            OptionLine = [j, sj, EOS_DIR, AFS_DIR, f,MaxDOCA,MaxAngle,MaxSTG,MaxSLG]
-   #            SHName = AFS_DIR + '/HTCondor/SH/SH_E6_' + str(j) + '_' + str(sj) + '_' + str(f)+'.sh'
-   #            SUBName = AFS_DIR + '/HTCondor/SUB/SUB_E6_' + str(j) + '_' + str(sj) + '_' + str(f)+'.sub'
-   #            MSGName = AFS_DIR + '/HTCondor/MSG/MSG_E6_' + str(j) + '_' + str(sj)+'_' + str(f)
-   #            ScriptName = AFS_DIR + '/Code/Utilities/E6_DecorateFakeTracks_Sub.py '
-   #            job_details=[OptionHeader, OptionLine, SHName, SUBName, MSGName, ScriptName, 1, 'EDER-TSU-E6', False,
-   #               False]
-   #            if os.path.isfile(required_output_file_location)!=True and os.path.isfile(new_output_file_location):
-   #               bad_pop.append(job_details)
-   #            else:
-   #             continue
-   #if len(bad_pop)>0:
-     # print(UF.TimeStamp(),bcolors.WARNING+'Warning, there are still', len(bad_pop), 'HTCondor jobs remaining'+bcolors.ENDC)
-     # print(bcolors.BOLD+'If you would like to wait and try again later please enter W'+bcolors.ENDC)
-     # print(bcolors.BOLD+'If you would like to resubmit please enter R'+bcolors.ENDC)
-     # UserAnswer=input(bcolors.BOLD+"Please, enter your option\n"+bcolors.ENDC)
-     # if UserAnswer=='W':
-     #     print(UF.TimeStamp(),'OK, exiting now then')
-     #     exit()
-     # if UserAnswer=='R':
-     #    for bp in bad_pop:
-     #        UF.SubmitJobs2Condor(bp)
-     #    print(UF.TimeStamp(), bcolors.OKGREEN+"All jobs have been resubmitted"+bcolors.ENDC)
-     #    print(bcolors.BOLD+"Please check them in few hours"+bcolors.ENDC)
-     #    exit()
-   #else:
-   print(UF.TimeStamp(),bcolors.OKGREEN+'All HTCondor Seed Creation jobs have finished'+bcolors.ENDC)
-   created_file=False
    for j in range(0,len(data)):
-       progress=round((float(j)/float(len(data)))*100,2)
-       print(UF.TimeStamp(),'Compressing output, progress is ',progress,' %', end="\r", flush=True) #Progress display
-       for sj in range(0,int(data[j][2])):
+        for sj in range(0,int(data[j][2])):
             for f in range (0,1000):
                new_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E5_E6_RawTracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
                required_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E6_E6_Dec_Fake_Tracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
-               if os.path.isfile(new_output_file_location):
-                   if created_file==False:
-                      track_data=pd.read_csv(required_output_file_location,usecols=['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])
-                      track_data['DOCA'] = track_data['DOCA'].astype(float)
-                      track_data['DOCA']=(track_data['DOCA']/DOCABin)
-                      track_data['DOCA']=track_data['DOCA'].apply(np.ceil)
-                      track_data['DOCA']=track_data['DOCA']*DOCABin
-                      
-                      track_data['Seg_Lon_Gap'] = track_data['Seg_Lon_Gap'].astype(float)
-                      track_data['Seg_Lon_Gap']=(track_data['Seg_Lon_Gap']/SLGBin)
-                      track_data['Seg_Lon_Gap']=track_data['Seg_Lon_Gap'].apply(np.ceil)
-                      track_data['Seg_Lon_Gap']=track_data['Seg_Lon_Gap']*SLGBin
-                      
-                      track_data['Seg_Transv_Gap'] = track_data['Seg_Transv_Gap'].astype(float)
-                      track_data['Seg_Transv_Gap']=(track_data['Seg_Transv_Gap']/STGBin)
-                      track_data['Seg_Transv_Gap']=track_data['Seg_Transv_Gap'].apply(np.ceil)
-                      track_data['Seg_Transv_Gap']=track_data['Seg_Transv_Gap']*STGBin
+               OptionHeader = [' --Set ', ' --SubSet ', ' --EOS ', " --AFS ", " --Fraction ",' --MaxDOCA ', ' --MaxAngle ', ' --MaxSTG ', ' --MaxSLG ']
+               OptionLine = [j, sj, EOS_DIR, AFS_DIR, f,MaxDOCA,MaxAngle,MaxSTG,MaxSLG]
+               SHName = AFS_DIR + '/HTCondor/SH/SH_E6_' + str(j) + '_' + str(sj) + '_' + str(f)+'.sh'
+               SUBName = AFS_DIR + '/HTCondor/SUB/SUB_E6_' + str(j) + '_' + str(sj) + '_' + str(f)+'.sub'
+               MSGName = AFS_DIR + '/HTCondor/MSG/MSG_E6_' + str(j) + '_' + str(sj)+'_' + str(f)
+               ScriptName = AFS_DIR + '/Code/Utilities/E6_DecorateFakeTracks_Sub.py '
+               job_details=[OptionHeader, OptionLine, SHName, SUBName, MSGName, ScriptName, 1, 'EDER-TSU-E6', False,
+                  False]
+               if os.path.isfile(required_output_file_location)!=True and os.path.isfile(new_output_file_location):
+                  bad_pop.append(job_details)
+               else:
+                continue
+   if len(bad_pop)>0:
+      print(UF.TimeStamp(),bcolors.WARNING+'Warning, there are still', len(bad_pop), 'HTCondor jobs remaining'+bcolors.ENDC)
+      print(bcolors.BOLD+'If you would like to wait and try again later please enter W'+bcolors.ENDC)
+      print(bcolors.BOLD+'If you would like to resubmit please enter R'+bcolors.ENDC)
+      UserAnswer=input(bcolors.BOLD+"Please, enter your option\n"+bcolors.ENDC)
+      if UserAnswer=='W':
+          print(UF.TimeStamp(),'OK, exiting now then')
+          exit()
+      if UserAnswer=='R':
+         for bp in bad_pop:
+             UF.SubmitJobs2Condor(bp)
+         print(UF.TimeStamp(), bcolors.OKGREEN+"All jobs have been resubmitted"+bcolors.ENDC)
+         print(bcolors.BOLD+"Please check them in few hours"+bcolors.ENDC)
+         exit()
+   else:
+       print(UF.TimeStamp(),bcolors.OKGREEN+'All HTCondor Seed Creation jobs have finished'+bcolors.ENDC)
+       created_file=False
+       for j in range(0,len(data)):
+           progress=round((float(j)/float(len(data)))*100,2)
+           print(UF.TimeStamp(),'Compressing output, progress is ',progress,' %', end="\r", flush=True) #Progress display
+           for sj in range(0,int(data[j][2])):
+                for f in range (0,1000):
+                   new_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E5_E6_RawTracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
+                   required_output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E6_E6_Dec_Fake_Tracks_'+str(j)+'_'+str(sj)+'_'+str(f)+'.csv'
+                   if os.path.isfile(new_output_file_location):
+                       if created_file==False:
+                          track_data=pd.read_csv(required_output_file_location,usecols=['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])
+                          track_data['DOCA'] = track_data['DOCA'].astype(float)
+                          track_data['DOCA']=(track_data['DOCA']/DOCABin)
+                          track_data['DOCA']=track_data['DOCA'].apply(np.ceil)
+                          track_data['DOCA']=track_data['DOCA']*DOCABin
 
-                      track_data['angle'] = track_data['angle'].astype(float)
-                      track_data['angle']=(track_data['angle']/AngleBin)
-                      track_data['angle']=track_data['angle'].apply(np.ceil)
-                      track_data['angle']=track_data['angle']*AngleBin
-                      track_data['angle']=track_data['angle'].abs()
-                      track_data['tracks']=1
-                      track_data=track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])['tracks'].sum().reset_index()
-                      track_data['track_type']='Fake'
-                      created_file=True
-                   else:
-                      new_track_data=pd.read_csv(required_output_file_location,usecols=['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])
-                      new_track_data['DOCA'] = new_track_data['DOCA'].astype(float)
-                      new_track_data['DOCA']=(new_track_data['DOCA']/DOCABin)
-                      new_track_data['DOCA']=new_track_data['DOCA'].apply(np.ceil)
-                      new_track_data['DOCA']=new_track_data['DOCA']*DOCABin
+                          track_data['Seg_Lon_Gap'] = track_data['Seg_Lon_Gap'].astype(float)
+                          track_data['Seg_Lon_Gap']=(track_data['Seg_Lon_Gap']/SLGBin)
+                          track_data['Seg_Lon_Gap']=track_data['Seg_Lon_Gap'].apply(np.ceil)
+                          track_data['Seg_Lon_Gap']=track_data['Seg_Lon_Gap']*SLGBin
 
-                      new_track_data['Seg_Lon_Gap'] = new_track_data['Seg_Lon_Gap'].astype(float)
-                      new_track_data['Seg_Lon_Gap']=(new_track_data['Seg_Lon_Gap']/SLGBin)
-                      new_track_data['Seg_Lon_Gap']=new_track_data['Seg_Lon_Gap'].apply(np.ceil)
-                      new_track_data['Seg_Lon_Gap']=new_track_data['Seg_Lon_Gap']*SLGBin
+                          track_data['Seg_Transv_Gap'] = track_data['Seg_Transv_Gap'].astype(float)
+                          track_data['Seg_Transv_Gap']=(track_data['Seg_Transv_Gap']/STGBin)
+                          track_data['Seg_Transv_Gap']=track_data['Seg_Transv_Gap'].apply(np.ceil)
+                          track_data['Seg_Transv_Gap']=track_data['Seg_Transv_Gap']*STGBin
 
-                      new_track_data['Seg_Transv_Gap'] = new_track_data['Seg_Transv_Gap'].astype(float)
-                      new_track_data['Seg_Transv_Gap']=(new_track_data['Seg_Transv_Gap']/STGBin)
-                      new_track_data['Seg_Transv_Gap']=new_track_data['Seg_Transv_Gap'].apply(np.ceil)
-                      new_track_data['Seg_Transv_Gap']=new_track_data['Seg_Transv_Gap']*STGBin
+                          track_data['angle'] = track_data['angle'].astype(float)
+                          track_data['angle']=(track_data['angle']/AngleBin)
+                          track_data['angle']=track_data['angle'].apply(np.ceil)
+                          track_data['angle']=track_data['angle']*AngleBin
+                          track_data['angle']=track_data['angle'].abs()
+                          track_data['tracks']=1
+                          track_data=track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])['tracks'].sum().reset_index()
+                          track_data['track_type']='Fake'
+                          created_file=True
+                       else:
+                          new_track_data=pd.read_csv(required_output_file_location,usecols=['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])
+                          new_track_data['DOCA'] = new_track_data['DOCA'].astype(float)
+                          new_track_data['DOCA']=(new_track_data['DOCA']/DOCABin)
+                          new_track_data['DOCA']=new_track_data['DOCA'].apply(np.ceil)
+                          new_track_data['DOCA']=new_track_data['DOCA']*DOCABin
 
-                      new_track_data['angle']=new_track_data['angle'].abs()
-                      new_track_data['angle'] = new_track_data['angle'].astype(float)
-                      new_track_data['angle']=(new_track_data['angle']/AngleBin)
-                      new_track_data['angle']=new_track_data['angle'].apply(np.ceil)
-                      new_track_data['angle']=new_track_data['angle']*AngleBin
+                          new_track_data['Seg_Lon_Gap'] = new_track_data['Seg_Lon_Gap'].astype(float)
+                          new_track_data['Seg_Lon_Gap']=(new_track_data['Seg_Lon_Gap']/SLGBin)
+                          new_track_data['Seg_Lon_Gap']=new_track_data['Seg_Lon_Gap'].apply(np.ceil)
+                          new_track_data['Seg_Lon_Gap']=new_track_data['Seg_Lon_Gap']*SLGBin
 
-                      new_track_data['tracks']=1
-                      new_track_data=new_track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])['tracks'].sum().reset_index()
-                      new_track_data['track_type']='Fake'
+                          new_track_data['Seg_Transv_Gap'] = new_track_data['Seg_Transv_Gap'].astype(float)
+                          new_track_data['Seg_Transv_Gap']=(new_track_data['Seg_Transv_Gap']/STGBin)
+                          new_track_data['Seg_Transv_Gap']=new_track_data['Seg_Transv_Gap'].apply(np.ceil)
+                          new_track_data['Seg_Transv_Gap']=new_track_data['Seg_Transv_Gap']*STGBin
 
-                      combo_track_data = [track_data,new_track_data]
+                          new_track_data['angle']=new_track_data['angle'].abs()
+                          new_track_data['angle'] = new_track_data['angle'].astype(float)
+                          new_track_data['angle']=(new_track_data['angle']/AngleBin)
+                          new_track_data['angle']=new_track_data['angle'].apply(np.ceil)
+                          new_track_data['angle']=new_track_data['angle']*AngleBin
 
-                      track_data = pd.concat(combo_track_data)
-                      track_data=track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle','track_type'])['tracks'].sum().reset_index()
-   print(track_data)
-   exit()
-#    print(UF.TimeStamp(),'Cleaning up the work space... ',bcolors.ENDC)
-#        UF.EvalCleanUp(AFS_DIR, EOS_DIR, 'E6', ['E5_E6'], "SoftUsed == \"EDER-VIANN-E6\"")
-#        print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
-#        print(UF.TimeStamp(), bcolors.OKGREEN+"Fake 2-track decoration is completed"+bcolors.ENDC)
-#        print(bcolors.HEADER+"############################################# End of the program ################################################"+bcolors.ENDC)
-# #End of the script
+                          new_track_data['tracks']=1
+                          new_track_data=new_track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])['tracks'].sum().reset_index()
+                          new_track_data['track_type']='Fake'
+
+                          combo_track_data = [track_data,new_track_data]
+
+                          track_data = pd.concat(combo_track_data)
+                          track_data=track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle','track_type'])['tracks'].sum().reset_index()
+       print(UF.TimeStamp(),'Adding genuine data from', bcolors.OKGREEN+EOS_DIR+'/EDER-TSU/Data/TEST_SET/E3_TRUTH_TRACKS.csv'+bcolors.ENDC)
+       truth_track_data=pd.read_csv(EOS_DIR+'/EDER-TSU/Data/TEST_SET/E3_TRUTH_TRACKS.csv',usecols=['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])
+
+       truth_track_data['DOCA'] = truth_track_data['DOCA'].astype(float)
+       truth_track_data['DOCA']=(truth_track_data['DOCA']/DOCABin)
+       truth_track_data['DOCA']=truth_track_data['DOCA'].apply(np.ceil)
+       truth_track_data['DOCA']=truth_track_data['DOCA']*DOCABin
+
+       truth_track_data['Seg_Lon_Gap'] = truth_track_data['Seg_Lon_Gap'].astype(float)
+       truth_track_data['Seg_Lon_Gap']=(truth_track_data['Seg_Lon_Gap']/SLGBin)
+       truth_track_data['Seg_Lon_Gap']=truth_track_data['Seg_Lon_Gap'].apply(np.ceil)
+       truth_track_data['Seg_Lon_Gap']=truth_track_data['Seg_Lon_Gap']*SLGBin
+
+       truth_track_data['Seg_Transv_Gap'] = truth_track_data['Seg_Transv_Gap'].astype(float)
+       truth_track_data['Seg_Transv_Gap']=(truth_track_data['Seg_Transv_Gap']/STGBin)
+       truth_track_data['Seg_Transv_Gap']=truth_track_data['Seg_Transv_Gap'].apply(np.ceil)
+       truth_track_data['Seg_Transv_Gap']=truth_track_data['Seg_Transv_Gap']*STGBin
+
+       truth_track_data['angle']=truth_track_data['angle'].abs()
+       truth_track_data['angle'] = truth_track_data['angle'].astype(float)
+       truth_track_data['angle']=(truth_track_data['angle']/AngleBin)
+       truth_track_data['angle']=truth_track_data['angle'].apply(np.ceil)
+       truth_track_data['angle']=truth_track_data['angle']*AngleBin
+
+       truth_track_data['tracks']=1
+       truth_track_data=truth_track_data.groupby(['DOCA','Seg_Lon_Gap','Seg_Transv_Gap','angle'])['tracks'].sum().reset_index()
+       truth_track_data['track_type']='Genuine'
+       combo_track_data = [track_data,truth_track_data]
+       track_data = pd.concat(combo_track_data)
+       output_file_location=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E6_FAKE_GENUINE_TRACKS.csv'
+       print(UF.TimeStamp(),'Writing combined data into', bcolors.OKGREEN+output_file_location+bcolors.ENDC)
+       track_data.to_csv(output_file_location,index=False)
+       print(UF.TimeStamp(),'Cleaning up the work space... ',bcolors.ENDC)
+       #UF.EvalCleanUp(AFS_DIR, EOS_DIR, 'E6', ['E5_E6','E6_E6'], "SoftUsed == \"EDER-TSU-E6\"")
+       print(bcolors.HEADER+"########################################################################################################"+bcolors.ENDC)
+       print(UF.TimeStamp(), bcolors.OKGREEN+"Fake 2-track decoration is completed"+bcolors.ENDC)
+       print(bcolors.HEADER+"############################################# End of the program ################################################"+bcolors.ENDC)
+       #End of the script
 
 
 
