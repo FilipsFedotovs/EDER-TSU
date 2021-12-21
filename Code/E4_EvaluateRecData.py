593ed7ff21339f0b0fa2b73c602e5f4947c4d0fd
Filips <fedotovs@hotmail.com>
2021-12-21 18:01:27 +0000

    Development

--- Code/E4_EvaluateRecData.py
+++ Code/E4_EvaluateRecData.py
@@ -140,7 +140,7 @@ elif args.TypeOfAnalysis == 'ALL' or args.TypeOfAnalysis == 'FEDRA':
     print(seg_data)
     seg_data_segm_kpi=seg_data.drop(['FEDRA_Seg_No'],axis=1)
     seg_data_segm_kpi=seg_data_segm_kpi.groupby(by=['MC_Mother_Track_ID'])['FEDRA_Seg_ID'].count().reset_index()
-
+    print(seg_data_segm_kpi)
     print(seg_data_segm_kpi['MC_Mother_Track_ID'].nunique())
     print(seg_data_segm_kpi['FEDRA_Seg_ID'].sum())
     print(seg_data_segm_kpi['FEDRA_Seg_ID'].mean())
