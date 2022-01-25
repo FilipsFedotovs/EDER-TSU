202b0a47859dcd06318ea62f59649efa3445a3e6
Filips <fedotovs@hotmail.com>
2022-01-25 00:26:19 +0000

    Development

--- Code/R5_GlueTrackSegments.py
+++ Code/R5_GlueTrackSegments.py
@@ -181,14 +181,15 @@ if args.Mode=='C':
                      progress=round((float(SeedCounter)/float(len(VertexPool)))*100,0)
                      print(UF.TimeStamp(),'progress is ',progress,' %', end="\r", flush=True) #Progress display
                      SubjectSeed=VertexPool[SeedCounter]
-                     print(SubjectSeed.SegmentHeader)
-                     print(SubjectSeed.SegmentHits)
-                     print(SubjectSeed.TR_CNN_FIT)
+
 
                      for ObjectSeed in VertexPool[SeedCounter+1:]:
                                  print(ObjectSeed.SegmentHeader)
                                  print(ObjectSeed.SegmentHits)
                                  print(ObjectSeed.TR_CNN_FIT)
+                                 print(SubjectSeed.SegmentHeader)
+                                 print(SubjectSeed.SegmentHits)
+                                 print(SubjectSeed.TR_CNN_FIT)
                                  if SubjectSeed.InjectTrack(ObjectSeed):
                                              VertexPool.pop(VertexPool.index(ObjectSeed))
                      SeedCounter+=1
