b03a4a90de8c9269b4f38704053653c1fa82b49b
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2022-02-25 11:14:50 +0000

    Update R2_GenerateTracks.py

--- Code/R2_GenerateTracks.py
+++ Code/R2_GenerateTracks.py
@@ -150,7 +150,6 @@ if Mode=='C':
              print(UF.TimeStamp(),'Initiating the logging...')
              eval_data_file=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E3_TRUTH_TRACKS.csv'
              eval_data=pd.read_csv(eval_data_file,header=0,usecols=['Segment_1','Segment_2'])
-             
              eval_data["Track_ID"]= ['-'.join(sorted(tup)) for tup in zip(eval_data['Segment_1'], eval_data['Segment_2'])]
              eval_data.drop(['Segment_1'],axis=1,inplace=True)
              eval_data.drop(['Segment_2'],axis=1,inplace=True)
@@ -165,19 +164,14 @@ if Mode=='C':
                       else:
                          progress=round((float(j)/float(len(data)))*100,0)
                          print(UF.TimeStamp(),'progress is ',progress,' %', end="\r", flush=True) #Progress display
-                         
                          rec=pd.read_csv(new_input_file_location,usecols = ['Segment_1','Segment_2'])
                          rec["Track_ID"]= ['-'.join(sorted(tup)) for tup in zip(rec['Segment_1'], rec['Segment_2'])]
                          rec.drop(['Segment_1'],axis=1,inplace=True)
                          rec.drop(['Segment_2'],axis=1,inplace=True)
                          rec_eval=pd.merge(eval_data, rec, how="inner", on=['Track_ID'])
                          eval_no+=len(rec_eval)
-                         rec_no+=(len(rec)-len(rec_eval))
-                         print('1',eval_no)
-             
-             print([2,'SLG and STG cuts',rec_no,eval_no,eval_no/(rec_no+eval_no),eval_no/len(eval_data)])
-             exit()
-             #UF.LogOperations(EOS_DIR+'/EDER-TSU/Data/REC_SET/R_LOG.csv', 'UpdateLog', [['Step_No','Step_Desc','Fake_Seeds','Truth_Seeds','Precision','Recall'],[2,'SLG and STG cuts',rec_no,eval_no,eval_no/(rec_no+eval_no),1.0]])
+                         rec_no+=(len(rec)-len(rec_eval))           
+             UF.LogOperations(EOS_DIR+'/EDER-TSU/Data/REC_SET/R_LOG.csv', 'UpdateLog', [[2,'SLG and STG cuts',rec_no,eval_no,eval_no/(rec_no+eval_no),eval_no/len(eval_data)]])
              print(UF.TimeStamp(), bcolors.OKGREEN+"The log data has been created successfully and written to"+bcolors.ENDC, bcolors.OKBLUE+EOS_DIR+'/EDER-TSU/Data/REC_SET/R_LOG.csv'+bcolors.ENDC)
         #  except:
          #    exit()
