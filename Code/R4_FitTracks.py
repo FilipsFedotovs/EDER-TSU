1fb53d52ee589192892b198947eddf00dd584643
Filips Fedotovs <74325502+FilipsFedotovs@users.noreply.github.com>
2022-02-26 20:49:18 +0000

    Update R4_FitTracks.py

--- Code/R4_FitTracks.py
+++ Code/R4_FitTracks.py
@@ -182,7 +182,7 @@ if Mode=='C':
        pickle.dump(base_data,base_data_file)
        base_data_file.close()     
        if args.Log=='Y':
-         #try:
+         try:
              print(UF.TimeStamp(),'Initiating the logging...')
              eval_data_file=EOS_DIR+'/EDER-TSU/Data/TEST_SET/E3_TRUTH_TRACKS.csv'
              eval_data=pd.read_csv(eval_data_file,header=0,usecols=['Segment_1','Segment_2'])
@@ -207,8 +207,8 @@ if Mode=='C':
              else:
                  UF.LogOperations(EOS_DIR+'/EDER-TSU/Data/REC_SET/R_LOG.csv', 'UpdateLog', [[5,'CNN Postfit',rec_no,eval_no,eval_no/(rec_no+eval_no),eval_no/len(eval_data)]])              
              print(UF.TimeStamp(), bcolors.OKGREEN+"The log data has been created successfully and written to"+bcolors.ENDC, bcolors.OKBLUE+EOS_DIR+'/EDER-TSU/Data/REC_SET/R_LOG.csv'+bcolors.ENDC)
-         #except:
-          #   print(UF.TimeStamp(), bcolors.WARNING+'Log creation has failed'+bcolors.ENDC)
+         except:
+             print(UF.TimeStamp(), bcolors.WARNING+'Log creation has failed'+bcolors.ENDC)
        print(UF.TimeStamp(),'Cleaning up the work space... ',bcolors.ENDC)
        exit()
        UF.RecCleanUp(AFS_DIR, EOS_DIR, 'R4', ['R4_R4'], "SoftUsed == \"EDER-TSU-R4\"")
