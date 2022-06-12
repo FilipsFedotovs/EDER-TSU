b8cf850932a70da3cea0b1ac66a2a9ec484e422e
Filips <fedotovs@hotmail.com>
2022-06-12 16:47:10 +0100

    Add csv agg option

--- Code/E4_EvaluateRecData.py
+++ Code/E4_EvaluateRecData.py
@@ -141,9 +141,7 @@ if args.TypeOfAnalysis == 'ALL' or args.TypeOfAnalysis == 'TRACKING':
 
     seg_data_segm_kpi.to_csv(output_file_location,index=False)
     seg_data_segm_kpi=seg_data_segm_kpi.groupby(by=['FEDRA_Seg_ID'])['MC_Mother_Track_ID'].count().reset_index()
-    print(seg_data_segm_kpi)
-    exit()
-    output_file_location = EOS_DIR + '/EDER-TSU/Data/TEST_SET/E4_MC_TRACK_SEGMENTATION_STATS.csv'
+    output_file_location = EOS_DIR + '/EDER-TSU/Data/TEST_SET/E4_MC_TRACK_SEGMENTATION_STATS_AGG.csv'
     seg_data_segm_kpi.to_csv(output_file_location,index=False)
 
 
