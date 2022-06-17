d972eeda81811887af9da5fa40a8b7832619d6e0
WenqingXie47 <wenqing_xie@126.com>
2022-06-17 03:35:14 +0100

    PM

--- Code/bM1_PrepareTrainData.py
+++ Code/bM1_PrepareTrainData.py
@@ -53,7 +53,7 @@ print(UF.TimeStamp(), bcolors.OKGREEN+"Modules have been imported successfully..
 print(UF.TimeStamp(),'Loading raw data from',bcolors.OKBLUE+input_file_location+bcolors.ENDC)
 data=pd.read_csv(input_file_location,
             header=0,
-            usecols=[PM.FEDRA_Track_ID,PM.FEDRA_Track_QUADRANT,PM.x,PM.y,PM.z,PM.MC_Track_ID,PM.MC_Event_ID,MC_Mother_PDG])
+            usecols=[PM.FEDRA_Track_ID,PM.FEDRA_Track_QUADRANT,PM.x,PM.y,PM.z,PM.MC_Track_ID,PM.MC_Event_ID,PM.MC_Mother_PDG])
 
 total_rows=len(data.axes[0])
 print(UF.TimeStamp(),'The raw data has ',total_rows,' hits')
