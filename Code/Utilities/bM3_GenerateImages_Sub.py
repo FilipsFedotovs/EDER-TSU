0019db24689d9b04ebfa888437eadbdbf85fec58
WenqingXie47 <wenqing_xie@126.com>
2022-06-21 15:46:45 +0100

    n

--- Code/Utilities/bM3_GenerateImages_Sub.py
+++ Code/Utilities/bM3_GenerateImages_Sub.py
@@ -75,7 +75,7 @@ for s in range(0,limit):
     label=(track[1] in MotherPDGList)
     # for test
     #print(track[0], track[1], label)
-    track=Track(track[0])
+    track=Track([track[0]])
     if label:
         num_label = 1
     else:
