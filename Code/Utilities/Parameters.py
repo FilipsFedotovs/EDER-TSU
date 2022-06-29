ae30d322b8ba90290dad91053fc1a76fb8002d79
WenqingXie47 <wenqing_xie@126.com>
2022-06-29 20:21:37 +0100

    test 4

--- Code/Utilities/Parameters.py
+++ Code/Utilities/Parameters.py
@@ -42,7 +42,7 @@ if Config=='SND':
     Post_CNN_Model_Name='1T_50_SHIP_POSTFIT_1_model'
     #ModelArchitecture=[[6, 4, 1, 2, 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7, 1, 1, 4]]
     ModelArchitecture=[[1, 4, 1, 2, 2, 2, 2], [1, 4, 1, 2, 2, 2, 2], [1, 4, 1, 2, 2, 2, 2],[], [], [1, 4, 2], [], [], [], [], [7, 1, 1, 4]]
-    ModelArchitecturePlus=[[0.5, 4, [1, 1, 3], [8, 4, 32], 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7, 1, 1, 4]]
+    ModelArchitecturePlus=[[0.25, 4, [1, 1, 3], [16, 4, 32], 2, 2, 2], [], [],[], [], [1, 4, 2], [], [], [], [], [7, 1, 1, 4]]
 else:
     ########List of the package run parameters
     MaxSegmentsPerJob=15000 #This parameter imposes the limit on the number of the tracks form the Start plate when forming the Seeds.
