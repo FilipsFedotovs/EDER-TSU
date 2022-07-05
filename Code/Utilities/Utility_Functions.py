df82add0617b04068d665d4dd82a8931ac2c3fe6
WenqingXie47 <wenqing_xie@126.com>
2022-07-05 15:26:54 +0100

    .

--- Code/Utilities/Utility_Functions.py
+++ Code/Utilities/Utility_Functions.py
@@ -399,6 +399,8 @@ class Track:
                   __Hits[0]=float(__Hits[0])-__FinX
                   __Hits[1]=float(__Hits[1])-__FinY
                   __Hits[2]=float(__Hits[2])-__FinZ
+          print(__TempTrack.__Hits)
+          exit()
 
           #
           #Lon Rotate x
