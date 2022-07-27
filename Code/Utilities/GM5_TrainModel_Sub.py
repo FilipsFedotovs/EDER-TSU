8414260e0c95d47a8f304f402cbe31fee20d32e6
WenqingXie47 <wenqing_xie@126.com>
2022-07-27 12:54:48 +0100

    .

--- Code/Utilities/GM5_TrainModel_Sub.py
+++ Code/Utilities/GM5_TrainModel_Sub.py
@@ -186,15 +186,17 @@ def test(loader):
          out = model(data.x, data.edge_index, data.batch)  
          pred = out.argmax(dim=1)  # Use the class with highest probability.
          y_index = data.y.argmax(dim=1)
+         loss += criterion(out, data.y)
          correct += int((pred == y_index).sum())  # Check against ground-truth labels.
-     return correct / len(loader.dataset)  # Derive ratio of correct predictions.
+     return correct / len(loader.dataset), loss/len(loader.dataset)  # Derive ratio of correct predictions.
 
 
-for epoch in range(1, 171):
+for epoch in range(1, 40):
     train()
-    train_acc = test(train_loader)
-    test_acc = test(test_loader)
+    train_acc, train_loss = test(train_loader)
+    test_acc, test_loss = test(test_loader)
     print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+    print(f'Epoch: {epoch:03d}, Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}')
 
 exit()
 
