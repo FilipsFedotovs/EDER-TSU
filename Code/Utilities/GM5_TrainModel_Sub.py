c5c239ff5f8ac77bbdd6b7133dbd2b525b08cf93
WenqingXie47 <wenqing_xie@126.com>
2022-07-31 18:30:28 +0100

    .

--- Code/Utilities/GM5_TrainModel_Sub.py
+++ Code/Utilities/GM5_TrainModel_Sub.py
@@ -103,7 +103,7 @@ class GCN(torch.nn.Module):
         self.conv2 = GCNConv(hidden_channels, hidden_channels)
         self.conv3 = GCNConv(hidden_channels, hidden_channels)
         self.lin = Linear(hidden_channels, num_classes)
-        self.softmax = Softmax()
+        self.softmax = Softmax(dim=-1)
 
     def forward(self, x, edge_index, edge_attr, batch):
         # 1. Obtain node embeddings 
